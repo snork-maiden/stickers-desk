fde3ee4ab9034996ae0979c9ac63000f3a6d25bf
snork-maiden <pollykoldysheva@gmail.com>
2024-10-03 14:04:02 +0300

    Add pinia, add fonts, add 404 page styles

--- pages/[404].vue
+++ pages/[404].vue
@@ -1,41 +1,37 @@
 <template>
-    <section class="not-found-container">
-      <div class="not-found-message">
-        Страница не найдена
-      </div>
-    </section>
-  </template>
-  
-  <style scoped lang="scss">
-  .not-found-container {
-    position: relative;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: #f8f9fa;
-    overflow: hidden;
-  
-    &::before {
-      content: '404';
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      font-size: min(40vw, 30rem);
-      font-weight: bold;
-      color: rgba(65, 157, 160, 0.05); 
-      z-index: 0; 
-      pointer-events: none;
-      user-select: none;
-    }
+  <main class="not-found-container">
+    <div class="not-found-message">Страница не найдена</div>
+  </main>
+</template>
+
+<style scoped lang="scss">
+.not-found-container {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+}
+
+.not-found-message {
+  position: relative;
+  font-size: 3.3rem;
+  font-weight: 500;
+  text-transform: uppercase;
+  text-align: center;
+  color: #333;
+  &::before {
+    content: "404";
+    position: absolute;
+    top: 0%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: min(40vw, 30rem);
+    font-weight: 800;
+    color: rgba(65, 157, 160, 0.05);
+    z-index: 0;
+    pointer-events: none;
+    user-select: none;
   }
-  
-  .not-found-message {
-    font-size: 3rem;
-    font-weight: bold;
-    text-transform: uppercase;
-    color: #333;
-  }
-  </style>
-  
+}
+</style>
